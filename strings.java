e007672173faa41674ee3d0da14b810d7eb6fb89
sofiya-28 <sofiyamalik91@gmail.com>
2026-06-14 22:46:10 +0530

    Create strings.java

---
+++ strings.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class strings {
+    public static void main(String[] args){
+       /*  String name = new String( "i am sofiya");
+        System.out.println(name);
+        System.out.println("sofiya malik");
+        String college ="IMS Engineering college ";
+        System.out.println(college);*/
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the your name ");
+        String st =sc.next();
+        System.out.println(st);
+        sc.close();
+    }
+    
+}
