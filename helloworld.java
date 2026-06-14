4ce42006304577832c054132271eb5cbbb9b88c3
sofiya-28 <sofiyamalik91@gmail.com>
2026-06-14 21:01:42 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+public class helloworld {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+        System.out.println("I am learning java");
+        System.out.println("I always try to improve myself");
+        System.out.println("I am learning new things");
+    }
+    
+}
