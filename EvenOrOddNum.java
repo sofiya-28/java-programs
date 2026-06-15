1a3f45ef26e68908231fe61720f06e9829457610
sofiya-28 <sofiyamalik91@gmail.com>
2026-06-15 18:29:11 +0530

    Update EvenOrOddNum.java

--- EvenOrOddNum.java
+++ EvenOrOddNum.java
@@ -5,7 +5,7 @@ public class EvenOrOddNum {
         System.out.println("Enter the number");
         int num =sc.nextInt();
         if(num%2==0)
-             System.out.println(num+"Number is Even ");
+             System.out.println(num+" Number is Even ");
         else
             System.out.println(num+" Number is Odd ");
     }
