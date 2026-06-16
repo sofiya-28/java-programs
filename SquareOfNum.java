3cd21d4d2d2dfe875be52203325ac58e2714c55c
sofiya-28 <sofiyamalik91@gmail.com>
2026-06-16 08:48:43 +0530

    While.java
    basic program of loops

---
+++ SquareOfNum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class SquareOfNum {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=sc.nextInt();
+        System.out.println(n+ "square of number "+(n*n));
+        sc.close();
+        
+
+    }
+    
+}
