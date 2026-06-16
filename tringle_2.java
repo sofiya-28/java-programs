afe9087c0af57b2feb84637afbd6b3dfbfd79cf8
sofiya-28 <sofiyamalik91@gmail.com>
2026-06-16 18:24:39 +0530

    Create tringle_2.java

---
+++ tringle_2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class tringle_2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number of rows and columns ");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n+1-i;j++){
+                System.out.print("*");
+                
+            }
+        System.out.println();
+        }
+    sc.close();    
+    }
+    
+}
+    
+
