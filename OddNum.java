a87b7f01e4b597e7e70c5dfb95c8bfd6b26adcd5
sofiya-28 <sofiyamalik91@gmail.com>
2026-06-16 09:43:03 +0530

    OddNum.java

--- OddNum.java
+++ OddNum.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
 
-public class Evenno {
+public class OddNum {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Ente rthe number");
+        System.out.println("Enter the number ");
         int n=sc.nextInt();
         //method one
-       /*int i=1;
+
+       /* int i=1;
+        while(i<=n){
+            System.out.println(i);
+            i+=2;
+        }*/
+       //method second
+        int i=1;
         while(i<n){
-            if(i%2==0){
+            if(i%2!=0){
                 System.out.println(i);
             }
         i++;
-        }*/
-
-        //method second
-        int i=2;
-        while(i<n){
-            System.out.println(i);
-            i+=2;
-        }
-
+       }
     sc.close();
     }
     
