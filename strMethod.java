661bd02c7bb4d9a4738741317eed67b3accd2be9
sofiya-28 <sofiyamalik91@gmail.com>
2026-06-17 13:46:39 +0530

    Create strMethod.java

---
+++ strMethod.java
@@ -0,0 +1,17 @@
+public class strMethod {
+    public static void main(String[] args) {
+        String name ="SofiyaMalik"; 
+        System.out.println(name);
+
+        System.out.println(name.length());
+        
+        String lower=name.toLowerCase();
+        System.out.println(lower);
+
+        String upper = name.toUpperCase();
+        System.out.println(upper);
+
+
+    }
+    
+}
