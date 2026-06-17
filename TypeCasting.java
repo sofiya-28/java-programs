06253f29c68e63e381f834097aa65e8bd53586be
sofiya-28 <sofiyamalik91@gmail.com>
2026-06-17 20:02:22 +0530

    Create TypeCasting.java

---
+++ TypeCasting.java
@@ -0,0 +1,18 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+        //implicit typecasting
+        char ch='A';
+        int x=ch;
+        System.out.println(x);
+
+        // Explicit typecasting
+        char sofiya ='a';
+        int y = (int)sofiya;
+        System.out.println(y);
+         
+        // simple case : b assigne to s
+        char s='b';
+        System.out.println(s);
+    }
+    
+}
