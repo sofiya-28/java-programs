9ca0dd7d7d9905deccb7c69e1975961df7a0f95e
sofiya-28 <sofiyamalik91@gmail.com>
2026-06-24 22:33:29 +0530

    Create SumOfDigit.java
    sum of digit

---
+++ SumOfDigit.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SumOfDigit {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=sc.nextInt();
+        int sum =0;
+        while(n!=0){
+            sum += n%10;
+            n=n/10;
+        }
+        System.out.println(sum);
+    
+    sc.close();
+    }
+    
+}
