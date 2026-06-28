e90b33ce5c0fbca1296ec59201c357ea33d830fe
sofiya-28 <sofiyamalik91@gmail.com>
2026-06-28 23:20:35 +0530

    Create AlphabetsTriangle.java
    Alphabets Triangle pattern print

---
+++ AlphabetsTriangle.java
@@ -0,0 +1,11 @@
+public class AlphabetsTriangle {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++) {
+            for(char ch = 'A'; ch < 'A' + i; ch++) {
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
+    
+}
