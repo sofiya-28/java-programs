09d5cefb9f930164eaa9bb0a2ef193390a4b6880
sofiya-28 <sofiyamalik91@gmail.com>
2026-06-30 20:51:59 +0530

    Create AbcdPattern.java

---
+++ AbcdPattern.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class AbcdPattern {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print((char)(j+64)+" ");
+            }
+        System.out.println();
+        }
+    sc.close();
+    }
+
+}
