a0ff47eca181a9162a7bfe44837a3ef052bdf700
sofiya-28 <sofiyamalik91@gmail.com>
2026-07-01 22:27:54 +0530

    Create Mathod.java
    Basic code of Mathods

---
+++ Mathod.java
@@ -0,0 +1,17 @@
+public class Mathod {
+    public static void tum(){
+        System.out.println("Faiz");
+    }
+    public static void main(String[] args){
+        System.out.println("Sofiya");
+        tum();
+        aap();
+
+
+    }
+    public static void aap(){
+        System.out.println("Malik");
+        tum();
+    }
+    
+}
