5cfbf758d1ae219470e59cd73caed4fdce80db27
sofiya-28 <sofiyamalik91@gmail.com>
2026-07-02 19:16:44 +0530

    Update ReturnIsMandatory.java
    ReturnType

--- ReturnIsMandatory.java
+++ ReturnIsMandatory.java
@@ -1,8 +1,7 @@
 public class ReturnIsMandatory {
     public static void main(String[] args){
         System.out.println(sofiya(4) );
-
-
+        
     }
     public static int sofiya(int a){
         if(a<5) return a;
