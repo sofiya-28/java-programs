4128a6dbfffcda144df453fe6dc35e531e61f95c
sofiya-28 <sofiyamalik91@gmail.com>
2026-07-02 11:48:14 +0530

    Create FunctionSum.java
    Sum of No using Function

---
+++ FunctionSum.java
@@ -0,0 +1,9 @@
+public class FunctionSum {
+    public static void sum(int a,int b,int c){
+        System.out.println(a+b+c);
+    }
+    public static void main(String[] args){
+        sum(5,5,6);
+    }
+    
+}
