6e951b71a6488aeaa27c08b73f4e698c06f92a0d
sofiya-28 <sofiyamalik91@gmail.com>
2026-07-02 19:15:03 +0530

    BasicFunctionConcept.java
    Basic Concept of Function

---
+++ BasicFunctionConcept.java
@@ -0,0 +1,22 @@
+public class BasicFunctionConcept {
+    public static int tum(int a){
+        System.out.println("Sofiya");
+        if(a>5) return 1;
+        else return 0;
+    }
+    public static void main(String[] args){
+        System.out.println( tum(8));
+        aap();
+    }
+    public static int aap(){
+        System.out.println("Falik");
+        System.out.println("Faiz");
+    for(int i=1;i<=10;i++){
+        System.out.println(i);
+          
+        }
+        return 5;
+    
+    }
+    
+}
---
+++ ReturnIsMandatory.java
@@ -0,0 +1,14 @@
+public class ReturnIsMandatory {
+    public static void main(String[] args){
+        System.out.println(sofiya(4) );
+
+
+    }
+    public static int sofiya(int a){
+        if(a<5) return a;
+        else return 15;
+
+
+    }
+    
+}
