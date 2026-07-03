ad18021a420d5b77e9a6276eb210a6b48d9da82c
sofiya-28 <sofiyamalik91@gmail.com>
2026-07-03 19:18:05 +0530

    Create CreateArray.java
    Basic of Array

---
+++ Array/CreateArray.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class CreateArray {
+    public static void main(String[] args){
+        int[] x={10,20,30,40,50,60,70,80,90,100};
+        //indexing
+        System.out.println(x[5]);//access
+
+        //updating value
+        x[4]=45;
+        System.out.println(x[4]);
+
+    }
+
+    
+}
