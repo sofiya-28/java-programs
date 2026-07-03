d83097fa77b3895ec7be6d79d99e0078e6ad9e0e
sofiya-28 <sofiyamalik91@gmail.com>
2026-07-03 19:41:58 +0530

    Create PrintArray.java
    Print Element of Array and length

---
+++ Array/PrintArray.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class PrintArray {
+    public static void main(String[] args){
+        int[] arr ={10,20,30,40,50};
+        for(int i=0;i<=arr.length-1;i++){
+            System.out.print(arr[i]+" ");
+        }
+        // print length of array.
+        System.out.println(arr.length);
+
+    }
+    
+    
+}
