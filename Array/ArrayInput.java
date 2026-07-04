ea71ca2faf5641c48f61fe7c1135a9128750209c
sofiya-28 <sofiyamalik91@gmail.com>
2026-07-04 12:34:15 +0530

    Create ArrayInput.java
    Array Input

---
+++ Array/ArrayInput.java
@@ -0,0 +1,24 @@
+package Array;
+import java.util.Scanner;
+
+public class ArrayInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of Array");
+        int n=sc.nextInt();
+        int[] arr= new int[n];
+        System.out.println("Enter the elements in Array ");
+        for(int i=0;i<n;i++){
+            arr[i] =sc.nextInt();
+
+        }
+        for(int i=0;i<n;i++){
+        System.out.println(arr[i]);
+        }
+    
+    sc.close();
+
+
+    }
+    
+}
