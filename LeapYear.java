4146f14bd4034780de45ca5aed07f1086fc19c3b
sofiya-28 <sofiyamalik91@gmail.com>
2026-07-09 21:51:12 +0530

    Create LeapYear.java
    WAP to Check Year is Leap year or not

---
+++ LeapYear.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the year");
+        int year =sc.nextInt();
+        if(year%400==0)
+            System.out.println("Leap Year");
+        else if(year%100==0)
+            System.out.println("Not Leap Year");
+        else if(year%4==0)
+            System.out.println("Leap Year");
+        else
+            System.out.println("Not Leap Year");
+
+    sc.close();
+
+    }
+    
+}
