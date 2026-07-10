e9ba0c8984281ee021da886264b75ff638a0517d
sofiya-28 <sofiyamalik91@gmail.com>
2026-07-10 22:47:42 +0530

    Create Variable.java
    Declared and print variable

---
+++ Variable.java
@@ -0,0 +1,17 @@
+public class Variable {
+    public static void main(String[] args) {
+
+        int age = 20;
+        String name = "Sofiya";
+        double percentage = 87.5;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("Percentage : " + percentage);
+        System.out.println("Grade : " + grade);
+        System.out.println("Student : " + isStudent);
+
+    }
+}
