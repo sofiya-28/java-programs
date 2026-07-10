356cadf3651cec0752d248f524b6d2714d7cda0b
sofiya-28 <sofiyamalik91@gmail.com>
2026-07-11 00:08:14 +0530

    Create CalculatorUsingSwitch.java
    Two Number Simple Calculator program using Switch Statement.

---
+++ CalculatorUsingSwitch.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class CalculatorUsingSwitch {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the first number");
+        int n1=sc.nextInt();
+        System.out.println("Enter the operator");
+        char operator =sc.next().charAt(0);
+        System.out.println("enter the Second number");
+        int n2=sc.nextInt();
+
+        switch(operator){
+            case '+':
+            System.out.println(n1+n2);
+            break;
+            case '-':
+            System.out.println(n1-n2);
+            break;
+            case '*':
+            System.out.println(n1*n2);
+            break;       
+            case '/':
+            System.out.println(n1/n2);
+            break;
+            case '%':
+            System.out.println(n1%n2);
+            break;
+
+            default:
+                System.out.println("default value");
+
+        }
+    sc.close();
+    
+    }
+}
