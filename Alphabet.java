99b6a7cb5799a4cd5b5b8044de7c195ca3416df8
sofiya-28 <sofiyamalik91@gmail.com>
2026-07-13 22:22:34 +0530

    Create Alphabet.java
    print all alphabet

---
+++ Alphabet.java
@@ -0,0 +1,9 @@
+public class Alphabet {
+    public static void main(String[] args) {
+
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            System.out.print(ch + " ");
+        }
+    }
+    
+}
