445bc804328afaf044cf3e78cd7ce9439fce9391
sofiya-28 <sofiyamalik91@gmail.com>
2026-07-20 19:59:17 +0530

    Create CharArray.java
    String Array Print

---
+++ CharArray.java
@@ -0,0 +1,9 @@
+public class CharArray {
+    public static void main(String[] args){
+    
+        String[] arr= {"sofiya","Malik","Faiz","Insha","Falik","Shahanaz"};
+        for(int i=0;i<arr.length ;i++){
+            System.out.println(arr[i]+" ");
+        }
+    } 
+}
